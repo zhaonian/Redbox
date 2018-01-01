6ca38a1b896a9dd055c054edc3d7687146a99d94
Zhaonian Luan <zhaonial@uci.edu>
2018-01-01 09:57:34 -0800

    added cutomer route and split navbar view

--- Redbox/Controllers/MoviesController.cs
+++ Redbox/Controllers/MoviesController.cs
@@ -1,5 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
 using Redbox.Models;
+using Redbox.ViewModels;
 
 namespace Redbox.Controllers
 {
@@ -10,7 +13,38 @@ namespace Redbox.Controllers
         {
 
             var movie = new Movie() { Name = "Shrek!" };
-            return View();
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
+
+        //// movies
+        //public ActionResult index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (!String.IsNullOrEmpty(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
     }
 }
---
+++ Redbox/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Redbox.Models;
+
+namespace Redbox.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
